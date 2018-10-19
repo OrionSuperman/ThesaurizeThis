da1a721b0185e5ef5cd0c78a15b49f096ef9d3fd
Orion Supergan <orionsuperman@gmail.com>
2018-10-18 20:15:30 -0700

    initial setup of app. Working replies to test post

---
+++ app.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const Snoowrap = require('snoowrap');
+const Snoostorm = require('snoostorm');
+
+// Build Snoowrap and Snoostorm clients
+const r = new Snoowrap({
+    userAgent: 'thesaurize-this',
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    username: process.env.REDDIT_USER,
+    password: process.env.REDDIT_PASS
+});
+const client = new Snoostorm(r);
+
+// Configure options for stream: subreddit & results per query
+const streamOpts = {
+    subreddit: 'testingground4bots',
+    results: 25
+};
+  
+// Create a Snoostorm CommentStream with the specified options
+const comments = client.CommentStream(streamOpts);
+
+// On comment, perform whatever logic you want to do
+comments.on('comment', (comment) => {
+
+    if (comment.body.includes('!ThesaurizeThis')) {
+        comment.reply('Test complete');
+    }
+});
+  
