cc0418b85a1557bdc71acac5805e985fb6080c5b
Orion Supergan <orionsuperman@gmail.com>
2018-11-21 21:06:35 -0800

    updated ban list. updated common word arr

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const pluralize = require('pluralize');
 
 const globalCallWord = "!thesaurizethis";
 const fandango = "!dothefandango";
-const commonArr =["the","of","and","a","to","in","is","you","that","it","he","was","for","on","are","as","with","his","they","I","at","be","this","have","from","or","one","had","by","word","but","not","what","all","were","we","when","your","can","said","there","use","an","each","which","she","do","how","their","if","will","up","other","about","out","many","then","them","these","so","some","her","would","make","like","him","into","time","has","look","two","more","write","go","see","number","no","way","could","people","my","than","first","water","been","call","who","oil","its","now","find","long","down","day","did","get","come","made","may","part","i","me"];
+const commonArr =["the","of","and","a","to","in","is","you","that","it","he","was","for","on","are","as","with","his","they","I","at","be","this","have","from","or","one","had","by","word","but","not","what","all","were","we","when","your","can","said","there","use","an","each","which","she","do","how","their","if","will","up","about","out","many","then","them","these","so","some","her","would","make","like","him","into","time","has","look","two","more","go","see","no","way","could","my","than","been","call","who","its","now","did","get","come","made","may","part","i","me","his"];
 
 
 // Build Snoowrap and Snoostorm clients
@@ -199,7 +199,9 @@ function inBannedSub(subName){
         "r/tattoos",
         "r/aww",
         "r/nottheonion",
-        "r/animalsbeingbros"
+        "r/animalsbeingbros",
+        "r/jokes",
+        "r/nascar"
     ];
     
     return bannedSubs.includes(subName.toLowerCase());
